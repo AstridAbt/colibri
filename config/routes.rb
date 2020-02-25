cdaeb971b1068508e68536eb0cee2c24ac5e2ab0
Thomas Streich <tstreich@hotmail.fr>
2020-02-25 18:19:34 +0100

    ngrok-show-quests-css-not-finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :quests do
     resources :participations, only: [:create]
   end
-
-  resources :participations, only: [:index, :destroy]
-=======
-  resources :quests, only: [:index, :show]
->>>>>>> master
 end
