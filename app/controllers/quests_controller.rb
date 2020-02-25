99cad99a05f990900d3b9f8dc1bbde4429e42825
astrid aubert <astridaubert@gmail.com>
2020-02-25 15:18:41 +0100

    en cours

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,2 +1,13 @@
 class QuestsController < ApplicationController
+
+  def index
+    @quests = policy_scope(Quest)
+    authorize @quest
+  end
+
+  def show
+    @quest = Quest.find(params[:id])
+    authorize @quest
+  end
+
 end
