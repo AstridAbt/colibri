dc27f1fdc4717f3aa54c5bcdfda81fab35ff0a98
Thomas Streich <tstreich@hotmail.fr>
2020-02-26 11:02:08 +0100

    add columns to seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_122657) do
+ActiveRecord::Schema.define(version: 2020_02_26_095637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2020_02_25_122657) do
     t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "content_2"
+    t.text "content_3"
   end
 
   create_table "users", force: :cascade do |t|
