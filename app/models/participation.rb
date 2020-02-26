8efde199f5e25c1510898ef675324dc5ac705466
astrid aubert <astridaubert@gmail.com>
2020-02-26 15:59:01 +0100

    en cours

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -4,4 +4,11 @@ class Participation < ApplicationRecord
 
   scope :past, -> { where("? > ending_date", Date.today)}
   scope :current, -> { where("? BETWEEN starting_date AND ending_date", Date.today)}
+
+  def increment_user_score
+    new_quest_point = quest.points
+    updated_score = user.score + new_quest_point
+
+    user.update(score: updated_score)
+  end
 end
