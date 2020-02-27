a0127631722a3d971dd77cb8b7627696a52cdf26
astrid aubert <astridaubert@gmail.com>
2020-02-27 11:54:22 +0100

    profile show review integrated mes quêtes en cours et passées

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,8 @@ class ProfilesController < ApplicationController
   def show
     @profile = User.find(params[:id])
     authorize @profile
+    @participations = policy_scope(Participation)
+    @my_past_participations = @participations.pasted
+    @my_current_participations = @participations.currents
   end
 end
