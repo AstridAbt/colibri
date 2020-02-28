6d34ad57b4e8a6153ebfc5159eae525e2cf64bfe
Thomas Streich <tstreich@hotmail.fr>
2020-02-28 12:35:49 +0100

    javascript function-ready-to-go

--- app/javascript/components/initCollapse.js
+++ app/javascript/components/initCollapse.js
@@ -1,8 +1,24 @@
-function initCollapse() {
+const initCollapse = () => {
   const accordeon = document.getElementById("accordeonQuest");
-  // if (accordeon) {
-  // }
-    console.Log(accordeon);
-  }
+    if (accordeon) {
+      // chopper le quest_level
+      //
+      let collapse;
+      const questLevel = location.search.split('quest_level=')[1];
+      // En fonction de quest_level, déterminé le niveau 1, 2 ou 3 (faire des ifs)
+      // getElementById de l'élement à ouvir,
+      //
+      // Ajouter la class `show` à la liste des classe de cet élement
+      //
+      if (questLevel <= 100) {
+        collapse = document.getElementById('collapseOne');
+      } else if (questLevel >= 101 && questLevel <= 200) {
+        collapse = document.getElementById('collapseTwo');
 
-export default {initCollapse};
+      } else {
+        collapse = document.getElementById('collapseThree');
+      }
+      collapse.classList.add('show');
+    }
+};
+export { initCollapse };
