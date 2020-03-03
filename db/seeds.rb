65a8afd08f4c7490d79213d2cc845c204727677f
Jeremy Dumas <mulot90@hotmail.com>
2020-03-03 15:02:26 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -105,6 +105,8 @@ quest_1_6 = Quest.create!(
        Le thé en sachet individuel contient des microparticules de plastique",
     content_2: "
        Acheter du thé en vrac permet de réduire ses déchets"
+    content_3: "
+        Idéalement, il faut acheter du thé commerce équitable"
 )
 quest_1_6.photo.attach(io: file_1_6, filename: 'nes.png', content_type: 'image/png')
 
