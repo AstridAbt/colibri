a219c99d3f1012569814e3cafff212232ed16c34
Thomas Streich <tstreich@hotmail.fr>
2020-03-04 15:18:02 +0100

    seed not finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,13 @@ puts 'Finished users creation !'
 
 puts 'Creating quests...'
 
-file_1_1 = URI.open('')
+# file_1_1 = URI.open('')
 file_1_2 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795670/1_J_utilise_un_shampoing_solide_bio_qnxlpb.jpg')
 file_1_3 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795670/1_Je_fais_pipi_sous_la_douche_rvyvuh.jpg')
 file_1_4 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795670/1_Je_priviligie_les_escaliers_qbwq8v.jpg')
 file_1_5 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795670/1_J_utilise_un_moteur_de_recherche_%C3%A9co-responsable_temmat.jpg')
 file_1_6 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795669/1_J_utilise_une_boule_%C3%A0_th%C3%A9_brwmq1.jpg')
-file_2_1 = URI.open('')
+# file_2_1 = URI.open('')
 file_2_2 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795670/2_Je_r%C3%A9duis_ma_garde_robe_hbmfjp.jpg')
 file_2_3 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795670/2_Je_fabrique_une_%C3%A9ponge_z%C3%A9ro_d%C3%A9chet_a0zqy7.jpg')
 file_2_4 = URI.open('https://res.cloudinary.com/ddaiy1lcj/image/upload/v1582795670/2_Je_fais_des_cadeaux_r%C3%A9utilis%C3%A9s_zlcl6u.jpg')
